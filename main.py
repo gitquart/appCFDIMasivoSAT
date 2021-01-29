51ea4c3b9ff9827e756d4b70038fe09eba48532b
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-29 16:24:24 -0600

    ...

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #Tutorial de descarga CFDI masivamente : https://pypi.org/project/cfdiclient/
 import utils as tool
 
-token=tool.autenticacion()
+token=tool.descargarPaquete()
--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 from cfdiclient import Autenticacion
 from cfdiclient import Fiel
 from cfdiclient import SolicitaDescarga
+from cfdiclient import VerificaSolicitudDescarga
+from cfdiclient import DescargaMasiva
 import datetime
-
 import os
+import json
 
 FIEL_KEY = ''
 FIEL_CER = ''
 FIEL_PAS = 'chuy1987'
+rfc_solicitante = 'QCG190521ND3'
 pathToFiel=os.getcwd()+'\\FIEL'
 for file in os.listdir(pathToFiel):
     if file.endswith('.key'):
@@ -17,32 +20,52 @@ for file in os.listdir(pathToFiel):
         FIEL_CER=file
         continue 
 cer_der = open(pathToFiel+'\\'+FIEL_CER, 'rb').read()
-key_der = open(pathToFiel+'\\'+FIEL_KEY, 'rb').read()        
+key_der = open(pathToFiel+'\\'+FIEL_KEY, 'rb').read()  
+fiel = Fiel(cer_der, key_der, FIEL_PAS)      
 
 def autenticacion():
-    
-    fiel = Fiel(cer_der, key_der, FIEL_PAS)
     auth = Autenticacion(fiel)
     token = auth.obtener_token()
 
     return token
 
 def solicitaDescarga():
-    
-    fiel = Fiel(cer_der, key_der, FIEL_PAS)
+    #Ejemplo de respuesta  {'mensaje': 'Solicitud Aceptada', 'cod_estatus': '5000', 'id_solicitud': 'be2a3e76-684f-416a-afdf-0f9378c346be'}
     descarga = SolicitaDescarga(fiel)
-
     token = autenticacion()
-    rfc_solicitante = 'XAXX010101000'
-    fecha_inicial = datetime.datetime(2018, 1, 1)
-    fecha_final = datetime.datetime(2018, 12, 31)
-    rfc_emisor = 'XAXX010101000'
-    rfc_receptor = 'XAXX010101000'
+    fecha_inicial = datetime.datetime(2020, 1, 1)
+    fecha_final = datetime.datetime(2020, 12, 31)
+    rfc_emisor = 'QCG190521ND3'
+    rfc_receptor = 'QCG190521ND3'
     # Emitidos
     result = descarga.solicitar_descarga(token, rfc_solicitante, fecha_inicial, fecha_final, rfc_emisor=rfc_emisor)
-    print(result)
+    id_solicitud_emisor=result['id_solicitud']
     # Recibidos
     result = descarga.solicitar_descarga(token, rfc_solicitante, fecha_inicial, fecha_final, rfc_receptor=rfc_receptor)
+    id_solicitud_receptor=result['id_solicitud']
+    lsSolicitud=[id_solicitud_emisor,id_solicitud_receptor]
+
+    return lsSolicitud
+
+
+def verificaSolicitudDescarga():
+    #Ejemplo re respuesta  {'estado_solicitud': '3', 'numero_cfdis': '8', 'cod_estatus': '5000', 'paquetes': ['a4897f62-a279-4f52-bc35-03bde4081627_01'], 'codigo_estado_solicitud': '5000', 'mensaje': 'Solicitud Aceptada'}   
+    v_descarga = VerificaSolicitudDescarga(fiel)
+    token = autenticacion()
+    id_solicitud = solicitaDescarga()[0]
+    result = v_descarga.verificar_descarga(token, rfc_solicitante, id_solicitud)
+    lsPaquete=[result['paquetes']]
+
+    return lsPaquete
+
+def descargarPaquete():
+    #ejemplo de respuesta # {'cod_estatus': '', 'mensaje': '', 'paquete_b64': 'eyJhbG=='} 
+    descarga = DescargaMasiva(fiel)
+    token = autenticacion()
+    id_paquete = verificaSolicitudDescarga()[0]
+    result = descarga.descargar_paquete(token, rfc_solicitante, id_paquete)
     print(result)
+   
+    
 
 
