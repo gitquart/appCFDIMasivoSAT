ff5c52df98d216567c135008788a531f2feb8b26
Ulyses <ulyses.rico.2012@gmail.com>
2021-02-12 17:59:54 -0600

    ...

--- gui.py
+++ gui.py
@@ -43,7 +43,7 @@ notebook.add(fConfiguración, text='Configuración y proceso CFDI')
 
 #Título-Instrucciones
 lblInst=tk.Label(fConfiguración)
-ft = tkFont.Font(size=10)
+ft = tkFont.Font(size=10,weight=tkFont.BOLD)
 lblInst["font"] = ft
 lblInst["fg"] = "#333333"
 lblInst["justify"] = "center"
@@ -58,7 +58,7 @@ lblContInst["fg"] = "#333333"
 lblContInst["justify"] = "left"
 texto="1.Establece el directorio donde estarán los archivos FIEL, descarga y lectura de cfdi.zip"
 lblContInst["text"] = texto
-lblContInst.place(x=45,y=100,width=500,height=20)
+lblContInst.place(x=45,y=100,width=500,height=25)
   
 #lbl-Directorio
 lblDir=tk.Label(fConfiguración)
@@ -67,7 +67,7 @@ lblDir["font"] = ft
 lblDir["fg"] = "#333333"
 lblDir["justify"] = "center"
 lblDir["text"] = "Directorio:"
-lblDir.place(x=70,y=140,width=100,height=30)
+lblDir.place(x=70,y=140,width=100,height=25)
     
 #Caja de texto-directorio
 txtDir=tk.Entry(fConfiguración)
@@ -76,13 +76,12 @@ ft = tkFont.Font(size=10)
 txtDir["font"] = ft
 txtDir["fg"] = "#333333"
 txtDir["justify"] = "left"
-txtDir.place(x=160,y=140,width=272,height=30)
-#txtDir.insert(0,'Hola')
+txtDir.place(x=160,y=140,width=272,height=25)
     
 #Browser-directorio
 btnBrowser=tk.Button(fConfiguración,command=returnFolder)
 btnBrowser['text']='...'
-btnBrowser.place(x=440,y=140,width=50,height=30)
+btnBrowser.place(x=440,y=140,width=50,height=25)
 
 #Contenido-Instrucciones 2
 lblContInst2=tk.Label(fConfiguración)
@@ -92,13 +91,51 @@ lblContInst2["fg"] = "#333333"
 lblContInst2["justify"] = "left"
 texto='2. Dentro del directorio elegido, crea la carpeta "FIEL" con los siguientes archivos:\n\n'
 texto+='A) Archivo *.cer\nB) Archivo *.key\nC) Archivo "datos.txt" donde debes anotar,en dos líneas, RFC y constreseña de FIEL\n\n'
-texto+='3. Al tener los pasos 1 y 2 haz click en el botón "Soicitar CFDI" y sigue los mensajes \nposteriores'
+texto+='3. Introducir el rango de fechas del CFDI a recuperar\n'
+texto+='4. Hacer click en el botón "Soicitar CFDI" y sigue los mensajes posteriores'
 lblContInst2["text"] = texto
-lblContInst2.place(x=30,y=200,width=500,height=120)
+lblContInst2.place(x=40,y=200,width=500,height=120)
 
-#Browser-directorio
+#Rango de fechas
+#lbl Inicial
+lblFechaInicio=tk.Label(fConfiguración)
+ft = tkFont.Font(size=10)
+lblFechaInicio["font"] = ft
+lblFechaInicio["fg"] = "#333333"
+lblFechaInicio["justify"] = "left"
+lblFechaInicio["text"] = "Fecha inicial:"
+lblFechaInicio.place(x=50,y=340,width=100,height=25)
+
+#Caja de texto-fecha inicio
+txtFechaInicio=tk.Entry(fConfiguración)
+txtFechaInicio["borderwidth"] = "1px"
+ft = tkFont.Font(size=10)
+txtFechaInicio["font"] = ft
+txtFechaInicio["fg"] = "#333333"
+txtFechaInicio["justify"] = "left"
+txtFechaInicio.place(x=150,y=340,width=100,height=25)
+
+#lbl Final
+lblFechaFin=tk.Label(fConfiguración)
+ft = tkFont.Font(size=10)
+lblFechaFin["font"] = ft
+lblFechaFin["fg"] = "#333333"
+lblFechaFin["justify"] = "left"
+lblFechaFin["text"] = "Fecha fin:"
+lblFechaFin.place(x=60,y=380,width=100,height=25)
+
+#Caja de texto-fecha fin
+txtFechaFin=tk.Entry(fConfiguración)
+txtFechaFin["borderwidth"] = "1px"
+ft = tkFont.Font(size=10)
+txtFechaFin["font"] = ft
+txtFechaFin["fg"] = "#333333"
+txtFechaFin["justify"] = "left"
+txtFechaFin.place(x=150,y=380,width=100,height=25)
+
+#Btn solicitar
 btnSolicitar=tk.Button(fConfiguración,command=solicitarCFDI,text='Solicitar CFDI')
-btnSolicitar.place(x=230,y=350,width=100,height=30)
+btnSolicitar.place(x=230,y=420,width=100,height=25)
 
 
     
