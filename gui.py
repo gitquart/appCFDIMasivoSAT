f1e3299baf65454b036a320dd5fcc74f6b20d901
Ulyses <ulyses.rico.2012@gmail.com>
2021-02-12 15:59:13 -0600

    ...

--- gui.py
+++ gui.py
@@ -14,85 +14,74 @@ def showMessage(title,content):
     tkMessageBox.showinfo(title,content)
 
 
+# window window
+window = tk.Tk()
+#geometry=widthxheight
+window.geometry('600x600')
+window.resizable(width=False, height=False)
+window.title('Quart CFDI Wizard')
 
-def main():
-    # window window
-    window = tk.Tk()
-    #geometry=widthxheight
-    window.geometry('600x600')
-    window.resizable(width=False, height=False)
-    window.title('Quart CFDI Wizard')
+# create a notebook
+notebook = ttk.Notebook(window)
+notebook.pack(pady=10, expand=True)
+notebook.pack()
 
-    # create a notebook
-    notebook = ttk.Notebook(window)
-    notebook.pack(pady=10, expand=True)
-    notebook.pack()
+# create frames
+fConfiguración = ttk.Frame(notebook, width=590, height=590)
+fProceso = ttk.Frame(notebook, width=590, height=590)
 
-    # create frames
-    fConfiguración = ttk.Frame(notebook, width=590, height=590)
-    fProceso = ttk.Frame(notebook, width=590, height=590)
+fConfiguración.pack(fill='both', expand=True)
+fProceso.pack(fill='both', expand=True)
 
-    fConfiguración.pack(fill='both', expand=True)
-    fProceso.pack(fill='both', expand=True)
+# add frames to notebook
+notebook.add(fConfiguración, text='Coniguración')
+notebook.add(fProceso, text='Proceso CFDI')
 
-     # add frames to notebook
-    notebook.add(fConfiguración, text='Coniguración')
-    notebook.add(fProceso, text='Proceso CFDI')
+#Add content to Configuración
 
-    #Add content to Configuración
-
-    #Título-Instrucciones
-    lblInst=tk.Label(fConfiguración)
-    ft = tkFont.Font(size=10)
-    lblInst["font"] = ft
-    lblInst["fg"] = "#333333"
-    lblInst["justify"] = "center"
-    lblInst["text"] = "Instrucciones:"
-    lblInst.place(x=40,y=70,width=100,height=25)
+#Título-Instrucciones
+lblInst=tk.Label(fConfiguración)
+ft = tkFont.Font(size=10)
+lblInst["font"] = ft
+lblInst["fg"] = "#333333"
+lblInst["justify"] = "center"
+lblInst["text"] = "Instrucciones:"
+lblInst.place(x=40,y=70,width=100,height=25)
   
-
-    #Contenido-Instrucciones
-    lblContInst=tk.Label(fConfiguración)
-    ft = tkFont.Font(size=10)
-    lblContInst["font"] = ft
-    lblContInst["fg"] = "#333333"
-    lblContInst["justify"] = "left"
-    lblContInst["text"] = "Establece el directorio donde estarán los archivos FIEL, descarga y lectura de cfdi.zip"
-    lblContInst.place(x=45,y=100,width=500,height=20)
+#Contenido-Instrucciones
+lblContInst=tk.Label(fConfiguración)
+ft = tkFont.Font(size=10)
+lblContInst["font"] = ft
+lblContInst["fg"] = "#333333"
+lblContInst["justify"] = "left"
+lblContInst["text"] = "Establece el directorio donde estarán los archivos FIEL, descarga y lectura de cfdi.zip"
+lblContInst.place(x=45,y=100,width=500,height=20)
   
-
-    #lbl-Directorio
-    lblDir=tk.Label(fConfiguración)
-    ft = tkFont.Font(size=10)
-    lblDir["font"] = ft
-    lblDir["fg"] = "#333333"
-    lblDir["justify"] = "center"
-    lblDir["text"] = "Directorio:"
-    lblDir.place(x=70,y=140,width=100,height=30)
+#lbl-Directorio
+lblDir=tk.Label(fConfiguración)
+ft = tkFont.Font(size=10)
+lblDir["font"] = ft
+lblDir["fg"] = "#333333"
+lblDir["justify"] = "center"
+lblDir["text"] = "Directorio:"
+lblDir.place(x=70,y=140,width=100,height=30)
     
-
-    #Caja de texto-directorio
-    txtDir=tk.Entry(fConfiguración)
-    txtDir["borderwidth"] = "1px"
-    ft = tkFont.Font(size=10)
-    txtDir["font"] = ft
-    txtDir["fg"] = "#333333"
-    txtDir["justify"] = "left"
-    txtDir.place(x=160,y=140,width=272,height=30)
-    #txtDir.insert(0,'Hola')
+#Caja de texto-directorio
+txtDir=tk.Entry(fConfiguración)
+txtDir["borderwidth"] = "1px"
+ft = tkFont.Font(size=10)
+txtDir["font"] = ft
+txtDir["fg"] = "#333333"
+txtDir["justify"] = "left"
+txtDir.place(x=160,y=140,width=272,height=30)
+#txtDir.insert(0,'Hola')
     
-
-    #Browser-directorio
-    btnBrowser=tk.Button(fConfiguración,command=returnFolder)
-    btnBrowser['text']='...'
-    btnBrowser.place(x=440,y=140,width=50,height=30)
+#Browser-directorio
+btnBrowser=tk.Button(fConfiguración,command=returnFolder)
+btnBrowser['text']='...'
+btnBrowser.place(x=440,y=140,width=50,height=30)
     
-    window.mainloop()
+window.mainloop()
 
 
-
-
-
-
-if __name__=='__main__':
-    main()    
+  
