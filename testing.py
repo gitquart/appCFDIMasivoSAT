844a5d3ec70bbafcaf8552dfa5b8206ef18b9282
Ulyses <ulyses.rico.2012@gmail.com>
2021-04-14 12:29:50 -0500

    ...

--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
 from suds.client import Client
+import requests
 
-url='https://cfdidescargamasivasolicitud.clouda.sat.gob.mx'
-client = Client(url)
-print('...')
+#Example: How to send a WSDL request with body (XML)
+
+url = "https://httpbin.org/post"
+
+headers = {"content-type" : "application/soap+xml"}
+body = """
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:req="https://httpbin.org/post">
+   <soapenv:Header/>
+   <soapenv:Body/>
+</soapenv:Envelope>
+"""
+
+response = requests.post(url, data = body, headers = headers)
+print(response)
